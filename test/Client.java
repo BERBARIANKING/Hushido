0e5f641a32b49fd27c9aa3a8224c6bb235a6a88b
Nikolaos Bermparis <93055498+BERBARIANKING@users.noreply.github.com>
2024-06-19 22:44:12 +0300

    Create Client.java

---
+++ test/Client.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.net.*;
+
+public class Client {
+    public static void main(String[] args) {
+        String hostname = "192.168.1.1"; // IP Address of the server
+        int port = 1234;
+
+        try (Socket socket = new Socket(hostname, port)) {
+            OutputStream output = socket.getOutputStream();
+            PrintWriter writer = new PrintWriter(output, true);
+
+            Console console = System.console();
+            String text;
+
+            do {
+                text = console.readLine("Enter text: ");
+                writer.println(text);
+
+                InputStream input = socket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+                String response = reader.readLine();
+                System.out.println("Server response: " + response);
+            } while (!text.equals("bye"));
+
+        } catch (UnknownHostException ex) {
+            System.out.println("Server not found: " + ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println("I/O error: " + ex.getMessage());
+        }
+    }
+}
